0cd734bd830decd810b7095e1c187a591b04dd53
Luiz Cezer <lccezinha@gmail.com>
2020-07-21 17:04:07 -0300

    Setup config/prod

--- config/prod.exs
+++ config/prod.exs
@@ -1,3 +1,8 @@
 use Mix.Config
 
 config :giacobot, port: 80
+
+config :giacobot, Giacobot.Endpoint,
+  load_from_system_env: true,
+  http: [port: {:system, "PORT"}],
+  url: [host: "giacobot.gigalixirapp.com", port: 443]
