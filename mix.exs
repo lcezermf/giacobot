7fff60bc368d824157b392aaf836df0e25843125
Luiz Cezer <lccezinha@gmail.com>
2020-07-21 16:26:14 -0300

    Elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Giacobot.MixProject do
     [
       app: :giacobot,
       version: "0.1.0",
-      elixir: "~> 1.10",
+      elixir: "~> 1.5",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
