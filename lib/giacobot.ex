063b8819227cd2e16f97220c1867addb775b131d
Luiz Cezer <lccezinha@gmail.com>
2020-09-21 09:12:45 -0300

    Change some phrases

--- lib/giacobot.ex
+++ lib/giacobot.ex
@@ -1,20 +1,9 @@
 defmodule Giacobot do
 
   @phrases [
-    "Mazome",
-    "Meuzovo",
-    "2 X-elefante no romário lanches",
-    "Exemplo de cultura",
-    "Seloco",
-    "Lá em DV",
-    "TEU CU BURRO",
-    "TEU FIOTE",
-    "SARTE LôCO",
-    "Ainda bem que to na CIPA",
-    "Meu pau de rayban",
-    "Sarte nágua capivara",
-    "Pedra no rim é o djanho",
-    "Va lá pastar, seu cavalo de teta"
+    "Frase 1",
+    "Frase 2",
+    "Frase 3"
   ]
 
   def say, do: Enum.random(@phrases)
